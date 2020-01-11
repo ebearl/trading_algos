f7f326da83bbf803b858c05dfc65fe9bdce623c6
AndrewLBeasley <alloydbeasley@gmail.com>
2020-01-11 14:51:31 -0600

    history algorithm with extra series implementation

--- AlgoTrader/HistoryAlgorithm.cs
+++ AlgoTrader/HistoryAlgorithm.cs
@@ -30,7 +30,7 @@ namespace QuantConnect.Algorithm.CSharp
     /// This algorithm demonstrates the various ways you can call the History function,
     /// what it returns, and what you can do with the returned values.
     /// </summary>
-    public class HistoryAlgorithm : QCAlgorithm, IRegressionAlgorithmDefinition
+    public class test : QCAlgorithm, IRegressionAlgorithmDefinition
     {
         private int _count;
         private SimpleMovingAverage _spyDailySma;
@@ -42,29 +42,38 @@ namespace QuantConnect.Algorithm.CSharp
         public override void Initialize()
         {
             SetStartDate(2013, 10, 08);  //Set Start Date
-            SetEndDate(DateTime.Now);    //Set End Date
+            SetEndDate(2013, 10, 11);    //Set End Date
             SetCash(100000);             //Set Strategy Cash
-            SetBrokerageModel(BrokerageName.InteractiveBrokersBrokerage, AccountType.Cash);
-
-            var stockPlot = new Chart("Trade Plot");
-            var highPlots = new Series("High Price", SeriesType.Line, 0);
-            stockPlot.AddSeries(highPlots);
-            AddChart(stockPlot);
 
             // Find more symbols here: http://quantconnect.com/data
             var SPY = AddSecurity(SecurityType.Equity, "SPY", Resolution.Daily).Symbol;
-            //var DOW = AddSecurity(SecurityType.Equity, "DOW", Resolution.Daily).Symbol;
+            var CME_SP1 = AddData<QuandlFuture>("CHRIS/CME_SP1", Resolution.Daily).Symbol;
             // specifying the exchange will allow the history methods that accept a number of bars to return to work properly
-            //Securities["CHRIS/CME_SP1"].Exchange = new EquityExchange();
+            Securities["CHRIS/CME_SP1"].Exchange = new EquityExchange();
+
+            var stockPlot = new Chart("Trade Plot");
+            var assetPrice = new Series("Asset Price", SeriesType.Scatter, "$", Color.Red, ScatterMarkerSymbol.Triangle);
+            stockPlot.AddSeries(assetPrice);
+
 
             // we can get history in initialize to set up indicators and such
             _spyDailySma = new SimpleMovingAverage(14);
 
             // get the last calendar year's worth of SPY data at the configured resolution (daily)
             var tradeBarHistory = History<TradeBar>("SPY", TimeSpan.FromDays(365));
-            //var tradeBarHistory1 = History<TradeBar>("DOW", TimeSpan.FromDays(365));
             AssertHistoryCount("History<TradeBar>(\"SPY\", TimeSpan.FromDays(365))", tradeBarHistory, 250, SPY);
-            //AssertHistoryCount("History<TradeBar>(\"DOW\", TimeSpan.FromDays(365))", tradeBarHistory1, 250, DOW);
+
+            // get the last calendar day's worth of SPY data at the specified resolution
+            tradeBarHistory = History<TradeBar>("SPY", TimeSpan.FromDays(1), Resolution.Minute);
+            AssertHistoryCount("History<TradeBar>(\"SPY\", TimeSpan.FromDays(1), Resolution.Minute)", tradeBarHistory, 390, SPY);
+
+            // get the last 14 bars of SPY at the configured resolution (daily)
+            tradeBarHistory = History<TradeBar>("SPY", 14).ToList();
+            AssertHistoryCount("History<TradeBar>(\"SPY\", 14)", tradeBarHistory, 14, SPY);
+
+            // get the last 14 minute bars of SPY
+            tradeBarHistory = History<TradeBar>("SPY", 14, Resolution.Minute);
+            AssertHistoryCount("History<TradeBar>(\"SPY\", 14, Resolution.Minute)", tradeBarHistory, 14, SPY);
 
             // we can loop over the return value from these functions and we get TradeBars
             // we can use these TradeBars to initialize indicators or perform other math
@@ -73,20 +82,104 @@ namespace QuantConnect.Algorithm.CSharp
                 _spyDailySma.Update(tradeBar.EndTime, tradeBar.Close);
             }
 
-            // foreach (TradeBar tradeBar in tradeBarHistory1)
-            // {
-            // 	_spyDailySma.Update(tradeBar.EndTime, tradeBar.Close);
-            // }
+            // get the last calendar year's worth of quandl data at the configured resolution (daily)
+            var quandlHistory = History<QuandlFuture>("CHRIS/CME_SP1", TimeSpan.FromDays(365));
+            AssertHistoryCount("History<Quandl>(\"CHRIS/CME_SP1\", TimeSpan.FromDays(365))", quandlHistory, 250, CME_SP1);
+
+            // get the last 14 bars of SPY at the configured resolution (daily)
+            quandlHistory = History<QuandlFuture>("CHRIS/CME_SP1", 14);
+            AssertHistoryCount("History<Quandl>(\"CHRIS/CME_SP1\", 14)", quandlHistory, 14, CME_SP1);
+
+            // get the last 14 minute bars of SPY
+
+            // we can loop over the return values from these functions and we'll get Quandl data
+            // this can be used in much the same way as the tradeBarHistory above
+            _spyDailySma.Reset();
+            foreach (QuandlFuture quandl in quandlHistory)
+            {
+                _spyDailySma.Update(quandl.EndTime, quandl.Value);
+            }
+
+            // get the last year's worth of all configured Quandl data at the configured resolution (daily)
+            var allQuandlData = History<QuandlFuture>(TimeSpan.FromDays(365));
+            AssertHistoryCount("History<QuandlFuture>(TimeSpan.FromDays(365))", allQuandlData, 250, CME_SP1);
+
+            // get the last 14 bars worth of Quandl data for the specified symbols at the configured resolution (daily)
+            allQuandlData = History<QuandlFuture>(Securities.Keys, 14);
+            AssertHistoryCount("History<QuandlFuture>(Securities.Keys, 14)", allQuandlData, 14, CME_SP1);
+
+            // NOTE: using different resolutions require that they are properly implemented in your data type, since
+            //  Quandl doesn't support minute data, this won't actually work, but if your custom data source has
+            //  different resolutions, it would need to be implemented in the GetSource and Reader methods properly
+            //quandlHistory = History<QuandlFuture>("CHRIS/CME_SP1", TimeSpan.FromDays(7), Resolution.Minute);
+            //quandlHistory = History<QuandlFuture>("CHRIS/CME_SP1", 14, Resolution.Minute);
+            //allQuandlData = History<QuandlFuture>(TimeSpan.FromDays(365), Resolution.Minute);
+            //allQuandlData = History<QuandlFuture>(Securities.Keys, 14, Resolution.Minute);
+            //allQuandlData = History<QuandlFuture>(Securities.Keys, TimeSpan.FromDays(1), Resolution.Minute);
+            //allQuandlData = History<QuandlFuture>(Securities.Keys, 14, Resolution.Minute);
+
+            // get the last calendar year's worth of all quandl data
+            allQuandlData = History<QuandlFuture>(Securities.Keys, TimeSpan.FromDays(365));
+            AssertHistoryCount("History<QuandlFuture>(Securities.Keys, TimeSpan.FromDays(365))", allQuandlData, 250, CME_SP1);
+
+            // the return is a series of dictionaries containing all quandl data at each time
+            // we can loop over it to get the individual dictionaries
+            foreach (DataDictionary<QuandlFuture> quandlsDataDictionary in allQuandlData)
+            {
+                // we can access the dictionary to get the quandl data we want
+                var quandl = quandlsDataDictionary["CHRIS/CME_SP1"];
+            }
+
+            // we can also access the return value from the multiple symbol functions to request a single
+            // symbol and then loop over it
+            var singleSymbolQuandl = allQuandlData.Get("CHRIS/CME_SP1");
+            AssertHistoryCount("allQuandlData.Get(\"CHRIS/CME_SP1\")", singleSymbolQuandl, 250, CME_SP1);
+            foreach (QuandlFuture quandl in singleSymbolQuandl)
+            {
+                // do something with 'CHRIS/CME_SP1' quandl data
+            }
+
+            // we can also access individual properties on our data, this will
+            // get the 'CHRIS/CME_SP1' quandls like above, but then only return the Low properties
+            var quandlSpyLows = allQuandlData.Get("CHRIS/CME_SP1", "Low");
+            AssertHistoryCount("allQuandlData.Get(\"CHRIS/CME_SP1\", \"Low\")", quandlSpyLows, 250);
+            int lowLoopCount = 0;
+            foreach (decimal low in quandlSpyLows)
+            {
+            	if(lowLoopCount < 7) {
+
+            		var flagSeries = new Series($"LowP {lowLoopCount}", SeriesType.Flag, "$", Color.Red, ScatterMarkerSymbol.Triangle);
+            		stockPlot.AddSeries(flagSeries);
+            		Debug("add new Flag series");
+            	}
+
+                lowLoopCount++;
+            }
 
             // sometimes it's necessary to get the history for many configured symbols
 
             // request the last year's worth of history for all configured symbols at their configured resolutions
             var allHistory = History(TimeSpan.FromDays(365));
-            AssertHistoryCount("History(TimeSpan.FromDays(365))", allHistory, 250, SPY); //DOW);
+            AssertHistoryCount("History(TimeSpan.FromDays(365))", allHistory, 250, SPY, CME_SP1);
+
+            // request the last days's worth of history at the minute resolution
+            allHistory = History(TimeSpan.FromDays(1), Resolution.Minute);
+            AssertHistoryCount("History(TimeSpan.FromDays(1), Resolution.Minute)", allHistory, 391, SPY, CME_SP1);
+
+            // request the last 100 bars for the specified securities at the configured resolution
+            allHistory = History(Securities.Keys, 100);
+            AssertHistoryCount("History(Securities.Keys, 100)", allHistory, 100, SPY, CME_SP1);
+
+            // request the last 100 minute bars for the specified securities
+            allHistory = History(Securities.Keys, 100, Resolution.Minute);
+            AssertHistoryCount("History(Securities.Keys, 100, Resolution.Minute)", allHistory, 101, SPY, CME_SP1);
 
             // request the last calendar years worth of history for the specified securities
             allHistory = History(Securities.Keys, TimeSpan.FromDays(365));
-            AssertHistoryCount("History(Securities.Keys, TimeSpan.FromDays(365))", allHistory, 250, SPY);//, DOW);
+            AssertHistoryCount("History(Securities.Keys, TimeSpan.FromDays(365))", allHistory, 250, SPY, CME_SP1);
+            // we can also specify the resolution
+            allHistory = History(Securities.Keys, TimeSpan.FromDays(1), Resolution.Minute);
+            AssertHistoryCount("History(Securities.Keys, TimeSpan.FromDays(1), Resolution.Minute)", allHistory, 391, SPY, CME_SP1);
 
             // if we loop over this allHistory, we get Slice objects
             foreach (Slice slice in allHistory)
@@ -96,28 +189,21 @@ namespace QuantConnect.Algorithm.CSharp
                 // if those symbols were specified
             }
 
-            // we can access all the closing prices in chronological order using this get function
-            var closeHistorySPY = allHistory.Get("SPY", Field.Close);
-            //var closeHistoryDOW = allHistory.Get("DOW", Field.Close);
-            AssertHistoryCount("allHistory.Get(\"SPY\", Field.Close)", closeHistorySPY, 250);
-            //AssertHistoryCount("allHistory.Get(\"DOW\", Field.Close)", closeHistoryDOW, 390);
+            // we can access the history for individual symbols from the all history by specifying the symbol
+            // the type must be a trade bar!
+            tradeBarHistory = allHistory.Get<TradeBar>("SPY");
+            AssertHistoryCount("allHistory.Get(\"SPY\")", tradeBarHistory, 390, SPY);
 
-            foreach (decimal close in closeHistorySPY)
+            // we can access all the closing prices in chronological order using this get function
+            var closeHistory = allHistory.Get("SPY", Field.Close);
+            AssertHistoryCount("allHistory.Get(\"SPY\", Field.Close)", closeHistory, 390);
+            foreach (decimal close in closeHistory)
             {
                 // do something with each closing value in order
-                //horizontal array plots
-                var assetPrice = new Series("Price", SeriesType.Line, "$", Color.Green);
             }
 
-            // foreach (decimal close in closeHistoryDOW)
-            // {
-            // 	// do something with each closing value in order
-            // 	//horizontal array plots
-            // }
-
             // we can convert the close history into your normal double array (double[]) using the ToDoubleArray method
-            double[] doubleArray = closeHistorySPY.ToDoubleArray();
-            //double[] doubleArray2 = closeHistoryDOW.ToDoubleArray();
+            double[] doubleArray = closeHistory.ToDoubleArray();
 
             // for the purposes of regression testing, we're explicitly requesting history
             // using the universe symbols. Requests for universe symbols are filtered out
@@ -129,21 +215,7 @@ namespace QuantConnect.Algorithm.CSharp
                     + "These requests are intended to be filtered out and never sent to the history provider.");
             }
 
-
-
-
-
-
-        }
-
-        /// <summary>
-        /// OnEndOfDay Event Handler - At the end of each trading day we fire this code.
-        /// To avoid flooding, we recommend running your plotting at the end of each day.
-        /// </summary>
-        public override void OnEndOfDay()
-        {
-            //Log the end of day prices:
-            Plot("Trade Plot", "Price", trackPrice);
+            AddChart(stockPlot);
         }
 
         /// <summary>
@@ -153,19 +225,18 @@ namespace QuantConnect.Algorithm.CSharp
         public override void OnData(Slice data)
         {
             _count++;
-            trackPrice = data["SPY"].Close;
+            Plot("Trade Plot", "LowP {lowLoopCount}", data.Bars["SPY"].Close);
 
-            // if (_count > 5)
-            // {
-            //     throw new Exception("Invalid number of bars arrived. Expected exactly 5");
-            // }
+            if (_count > 5)
+            {
+                throw new Exception("Invalid number of bars arrived. Expected exactly 5");
+            }
 
-            // if (!Portfolio.Invested)
-            // {
-            //     SetHoldings("SPY", 1);
-            //     SetHoldings("DOW", 1);
-            //     Debug("Purchased Stock");
-            // }
+            if (!Portfolio.Invested)
+            {
+                SetHoldings("SPY", 1);
+                Debug("Purchased Stock");
+            }
         }
 
         private void AssertHistoryCount<T>(string methodCall, IEnumerable<T> history, int expected, params Symbol[] expectedSymbols)
@@ -185,6 +256,17 @@ namespace QuantConnect.Algorithm.CSharp
                     .Distinct()
                     .Where(sym => !expectedSymbols.Contains(sym))
                     .ToList();
+            }
+            else if (typeof(T).IsGenericType && typeof(T).GetGenericTypeDefinition() == typeof(DataDictionary<>))
+            {
+                if (typeof(T).GetGenericArguments()[0] == typeof(QuandlFuture))
+                {
+                    var dictionaries = (IEnumerable<DataDictionary<QuandlFuture>>) history;
+                    unexpectedSymbols = dictionaries.SelectMany(dd => dd.Keys)
+                        .Distinct()
+                        .Where(sym => !expectedSymbols.Contains(sym))
+                        .ToList();
+                }
             }
             else if (typeof(IBaseData).IsAssignableFrom(typeof(T)))
             {
@@ -249,5 +331,19 @@ namespace QuantConnect.Algorithm.CSharp
             {"Treynor Ratio", "0.868"},
             {"Total Fees", "$3.26"}
         };
+
+        /// <summary>
+        /// Custom quandl data type for setting customized value column name. Value column is used for the primary trading calculations and charting.
+        /// </summary>
+        public class QuandlFuture : Quandl
+        {
+            /// <summary>
+            /// Initializes a new instance of the <see cref="QuandlFuture"/> class.
+            /// </summary>
+            public QuandlFuture()
+                : base(valueColumnName: "Settle")
+            {
+            }
+        }
     }
 }
